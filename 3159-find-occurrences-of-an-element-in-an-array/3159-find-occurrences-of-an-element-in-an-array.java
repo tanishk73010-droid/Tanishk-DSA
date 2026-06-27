ae975f2c72800a9a6a29463f71703b423dce9ddf
Tanishk Singh <tanishk73010@gmail.com>
2026-06-27 14:02:32 +0530

    Time: 4 ms (96.86%), Space: 173.6 MB (13.84%) - LeetHub

--- 3159-find-occurrences-of-an-element-in-an-array/3159-find-occurrences-of-an-element-in-an-array.java
+++ 3159-find-occurrences-of-an-element-in-an-array/3159-find-occurrences-of-an-element-in-an-array.java
@@ -1,25 +1,27 @@
+import java.util.ArrayList;
+
 class Solution {
     public int[] occurrencesOfElement(int[] nums, int[] queries, int x) {
-        int count=0;
-        HashMap <Integer,Integer> map = new HashMap<>();
-        for(int i =0;i <nums.length;i++)
-        {
-            if(nums[i]==x){
-            count++;
-            map.put(count,i);
-            } }
-int [] arr = new int[queries.length];
-            for(int i=0;i<queries.length;i++)
-            {
-                if(queries[i]>count)
-                arr[i]=-1;
-                else
-                arr[i]=map.get(queries[i]);
-
-
-            }
-
-return arr;
+        ArrayList<Integer> occurrenceIndices = new ArrayList<>();
         
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == x) {
+                occurrenceIndices.add(i);
+            }
+        }
+        
+        int[] arr = new int[queries.length];
+        
+        for (int i = 0; i < queries.length; i++) {
+            int query = queries[i];
+            
+            if (query > occurrenceIndices.size()) {
+                arr[i] = -1;
+            } else {
+                arr[i] = occurrenceIndices.get(query - 1);
+            }
+        }
+        
+        return arr;
     }
 }
