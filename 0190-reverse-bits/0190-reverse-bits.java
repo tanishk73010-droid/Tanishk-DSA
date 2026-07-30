53ac72a7fca396935e97434629d21ac8efc48413
Tanishk Singh <tanishk73010@gmail.com>
2026-07-31 01:27:51 +0530

    Time: 1 ms (61.2%), Space: 42.5 MB (53.08%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -5,9 +5,9 @@ public class Solution {
         int ans = 0;
 
         for (int i = 0; i < 32; i++) {
-            // Check if the i-th bit of n is set
+            
             if ((n & (1 << i)) != 0) {
-                // Set the corresponding bit from the left
+                
                 ans |= (1 << (31 - i));
             }
         }
